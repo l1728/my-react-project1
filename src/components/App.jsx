17f31b0ddcbbd48ec735e2c6ef78cc4b2f021460
Tetyana Gogulya <cheburashka077@ukr.net>
2024-05-15 23:27:43 +0300

    lesson2_2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@
 //   );
 // };
 
-import { useState } from 'react';
+// import { useState } from 'react';
 
 // const App = () => {
 //   // let clicks = 0;
@@ -88,4 +88,17 @@ import { useState } from 'react';
 //   );
 // };
 
+import { useState, useEffect } from 'react';
+
+const App = () => {
+  const [clicks, setClicks] = useState(0);
+  useEffect(() => {
+    console.log('You can see me only once!');
+  }, []);
+  return (
+    <button onClick={() => setClicks(clicks + 1)}>
+      You clicked {clicks} times
+    </button>
+  );
+};
 export default App;
