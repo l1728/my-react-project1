0c4ef360573e2a4a1512c852459a7a73147a9ccf
Tetyana Gogulya <cheburashka077@ukr.net>
2024-06-23 23:58:31 +0300

    main_js

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,21 @@ import 'modern-normalize';
 import App from './components/App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { store, persistor } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <HelmetProvider>
+            <App />
+          </HelmetProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
